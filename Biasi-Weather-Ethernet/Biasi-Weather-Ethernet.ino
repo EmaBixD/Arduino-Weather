309c07e2223e161fe38cde2877b6e6f18d9d9b94
EmaBixD <156678932+EmaBixD@users.noreply.github.com>
2024-03-10 18:15:05 +0100

    Update Biasi-Weather-Ethernet.ino

--- Biasi-Weather-Ethernet/Biasi-Weather-Ethernet.ino
+++ Biasi-Weather-Ethernet/Biasi-Weather-Ethernet.ino
@@ -3,7 +3,7 @@
 ///    by Ivan & Emanuele    ///
 ////////////////////////////////
 
-// For Arduino UNO + AHT20 + BMP280 + Ethernet shield
+// For Arduino UNO + BMP280 + AHT20 + Ethernet shield
 
 #include <Adafruit_BMP280.h>
 #include <Adafruit_AHTX0.h>
